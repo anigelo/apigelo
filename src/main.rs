fa6da1bb79d6511cee7a85ab8defd4cc4a260e27
Miguel A. Arilla <mangelarilla@users.noreply.github.com>
2021-08-11 14:26:49 +0200

    docker build

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,13 @@ use actix_files::NamedFile;
 use actix_web::{get, web, App, HttpServer, Responder, Result, error};
 use crate::media::KitsuMediaCollection;
 use std::io::ErrorKind;
+use std::env;
 
-const MEDIA_PATH: &str = "/nas/nfs/docker/jellyfin/media/anime";
+const DEFAULT_MEDIA_PATH: &str = "/media";
 
 #[get("/media")]
 async fn get_media() -> impl Responder {
-    match server::media(MEDIA_PATH) {
+    match server::media(&get_media_path()) {
         Ok(media) => KitsuMediaCollection { data: Some(media) },
         _ => KitsuMediaCollection { data: None }
     }
@@ -18,7 +19,7 @@ async fn get_media() -> impl Responder {
 
 #[get("/media/{media_id}/episodes/{episode}/playback")]
 async fn playback(web::Path((media_id, episode)): web::Path<(String, String)>) -> Result<NamedFile> {
-    let episode_path = server::playback(MEDIA_PATH, media_id, episode)
+    let episode_path = server::playback(&get_media_path(), media_id, episode)
         .ok_or(std::io::Error::from(ErrorKind::NotFound))
         .map_err(|e| error::ErrorNotFound(e))?;
 
@@ -28,7 +29,11 @@ async fn playback(web::Path((media_id, episode)): web::Path<(String, String)>) -
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(get_media).service(playback))
-        .bind("127.0.0.1:8080")?
+        .bind("0.0.0.0:8080")?
         .run()
         .await
+}
+
+fn get_media_path() -> String {
+    env::var("MEDIA_PATH").unwrap_or(String::from(DEFAULT_MEDIA_PATH))
 }
